e54e242b960ac138bd5745ce5647da64129f5029
1711072 <104405279+1711072@users.noreply.github.com>
2022-06-08 10:55:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-music.setVolume(255)
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     music.playTone(262, music.beat(BeatFraction.Half))
     basic.pause(100)
     music.playTone(392, music.beat(BeatFraction.Half))
@@ -9,15 +8,25 @@ basic.forever(function () {
     music.playTone(523, music.beat(BeatFraction.Half))
     basic.pause(100)
 })
+input.onButtonPressed(Button.B, function () {
+    music.playTone(262, music.beat(BeatFraction.Sixteenth))
+    basic.pause(100)
+    music.playTone(392, music.beat(BeatFraction.Sixteenth))
+    basic.pause(100)
+    music.playTone(330, music.beat(BeatFraction.Sixteenth))
+    basic.pause(100)
+    music.playTone(523, music.beat(BeatFraction.Sixteenth))
+    basic.pause(100)
+})
+music.setVolume(255)
 basic.forever(function () {
-    serial.writeLine("" + (sonar.ping(
+    if (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-    )))
-    basic.showNumber(sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-    ))
+    ) > 10) {
+        basic.showString("A")
+    } else {
+        basic.showString("B")
+    }
 })
